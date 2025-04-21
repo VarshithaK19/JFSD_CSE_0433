1b33df88111cb50906e2db1512f6b08e14b38fa9
VarshithaK19 <Varshitha190819@gmail.com>
2025-04-21 11:43:13 +0530

    add files

---
+++ JAVA_Project/backend/Routes/ProductRouter.js
@@ -0,0 +1,10 @@
+const ensureAuthenticated = require('../Middlewares/Auth');
+
+const router = require('express').Router();
+
+router.get('/', ensureAuthenticated, (req, res) => {
+    console.log('---- logged in user detail ---', req.user);
+    res.status(200).json     
+});
+
+module.exports = router;
